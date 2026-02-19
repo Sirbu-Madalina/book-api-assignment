c10f04cacf04e5ea416a1b9ea6c544707323bc44
SirbuMadalina <madsir01@easv365.dk>
2026-02-19 12:53:14 +0100

    Server setup with routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import dotenvFlow from "dotenv-flow";
 
-dotenvFlow.config();
+import routes from './routes';
+
+//dotenvFlow.config();
 
 const app: Application = express();
+app.use('/api', routes);
 
 export function startServer() {
 
