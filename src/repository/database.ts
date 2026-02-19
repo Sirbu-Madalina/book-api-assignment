70ca02c92cf4d586efd11186add33c8c3b18f1d4
SirbuMadalina <madsir01@easv365.dk>
2026-02-19 17:57:16 +0100

    Errors

--- src/repository/database.ts
+++ src/repository/database.ts
@@ -1,21 +1,33 @@
 import mongoose from "mongoose";
 
+
 export async function connect() {
   try {
     if (!process.env.DBHOST) {
-      throw new Error("DBHOST environment variable is not defined");
+      throw new Error('DBHOST environment variable is not defined');
     }
     await mongoose.connect(process.env.DBHOST);
 
     if (mongoose.connection.db) {
       await mongoose.connection.db.admin().command({ ping: 1 });
-      console.log ("Connection established");
-    } 
-    else {
-      throw new Error("Database connection is not established");
+      console.log("Connection established");
     }
-  } 
+    else {
+      throw new Error('Database connection is not established');
+    }
+
+  }
   catch (error) {
-    console.log("Error connecting to the databse. Error: " + error);
+    console.log('Error connecting to the database. Error: ' + error);
+  }
+}
+
+export async function disconnect() {
+  try {
+    await mongoose.disconnect();
+    console.log ("Connection closed");
+  }
+  catch (error) {
+    console.log('Error closing database connection. Error: ' + error);
   }
 }
