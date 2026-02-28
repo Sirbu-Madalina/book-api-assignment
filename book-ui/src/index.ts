f458a23de7e9cf0be1a93da15939326412fa4912
SirbuMadalina <madsir01@easv365.dk>
2026-02-28 13:13:19 +0100

    restructure

--- book-ui/src/index.ts
+++ book-ui/src/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "./views/HomeView.vue";
 import LoginView from "./views/LoginView.vue";
+import { isLoggedIn } from "./services/auth";
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -8,4 +9,16 @@ export const router = createRouter({
     { path: "/", component: HomeView },
     { path: "/login", component: LoginView, meta: { layout: "auth" } },
   ],
+});
+
+router.beforeEach((to) => {
+  const logged = isLoggedIn();
+
+  // if not logged in, block Home
+  if (!logged && to.path === "/") return "/login";
+
+  // if logged in, block login page
+  if (logged && to.path === "/login") return "/";
+
+  return true;
 });
